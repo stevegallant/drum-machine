325c0bd287d948205b5e80e60005f4e25b88b947
Steve Gallant <sgallant4@gmail.com>
2020-09-06 14:56:50 -0400

    Split into modules. Add logic for sounds.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // My components
-
+import Drumpads from './modules/Drumpads';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,23 +13,11 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <div class="controls">
-          <h1 class="controls__label">Super Duper Drum Machine</h1>
-          <div id="display" class="controls__display">Display</div>
+        <div className="controls">
+          <h1 className="controls__label">Super Duper Drum Machine</h1>
+          <div id="display" className="controls__display">Display</div>
         </div>
-        <div class="btn-grid">
-          <div class="drum-pad">Q
-            <audio id="Q" class="clip" src="./assets/audio/crash.mp3">Audio element not supported in your browser.</audio>
-          </div>
-          <div class="drum-pad">W</div>
-          <div class="drum-pad">E</div>
-          <div class="drum-pad">A</div>
-          <div class="drum-pad">S</div>
-          <div class="drum-pad">D</div>
-          <div class="drum-pad">Z</div>
-          <div class="drum-pad">X</div>
-          <div class="drum-pad">C</div>
-        </div>
+        <Drumpads />
       </>
     );
   }
--- app/assets/scripts/modules/Drumpads.js
+++ app/assets/scripts/modules/Drumpads.js
@@ -1,1 +1,91 @@
 import React from 'react';
+import Pad from './Pad';
+
+class Drumpads extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', e => this.handleKeyPress(e));
+  }
+
+  handleClick(e) {
+    console.log("pad clicked " + e.target.innerText);
+    let audio = document.getElementById(e.target.innerText);
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+      audio.currentTime = 0;
+      audio.play();
+    };
+  }
+
+  handleKeyPress(e) {
+    console.log('key press ' + String.fromCharCode(e.keyCode));
+    let audio = document.getElementById(String.fromCharCode(e.keyCode));
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+      audio.currentTime = 0;
+      audio.play();
+    };
+  }
+
+  render() {
+    return (
+      <div className="btn-grid">
+        {Bank1.map((value, index) => {
+          return <Pad key={index} id={value.id} src={value.src} onClick={this.handleClick}/>
+        })}
+      </div>
+    );
+  }
+};
+
+
+
+const Bank1 = [
+  {
+    id: 'Q',
+    src: './assets/audio/hh-splash.mp3'
+  },
+  {
+    id: 'W',
+    src: './assets/audio/hh-closed.mp3'
+  },
+  {
+    id: 'E',
+    src: './assets/audio/cowbell.mp3'
+  },
+  {
+    id: 'A',
+    src: './assets/audio/hi-tom.mp3'
+  },
+  {
+    id: 'S',
+    src: './assets/audio/mid-tom.mp3'
+  },
+  {
+    id: 'D',
+    src: './assets/audio/low-tom.mp3'
+  },
+  {
+    id: 'Z',
+    src: './assets/audio/snare.mp3'
+  },
+  {
+    id: 'X',
+    src: './assets/audio/bass.mp3'
+  },
+  {
+    id: 'C',
+    src: './assets/audio/crash.mp3'
+  },
+];
+
+export default Drumpads;

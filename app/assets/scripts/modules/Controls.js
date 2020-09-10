408fcf4d22c84183b00e525c5900576fd7a0baf8
Steve Gallant <sgallant4@gmail.com>
2020-09-10 15:46:23 -0400

    Style volume slider input

--- app/assets/scripts/modules/Controls.js
+++ app/assets/scripts/modules/Controls.js
@@ -9,10 +9,9 @@ class Controls extends React.Component {
     return (
       <div className="controls">
         <h1 className="controls__label">Super Duper Drum Machine</h1>
-
-        <input type="range" id="vol" name="vol" min="0" max="1" step=".1" value=".5" />
+        <label htmlFor="" className="controls__label">Power</label>
         <label htmlFor="vol" className="controls__label">Volume</label>
-        
+        <input className="controls__vol" type="range" id="vol" name="vol" min="0" max="1" step=".1" value=".5" />
         <div id="display" className="controls__display">{this.props.displayText}</div>
       </div>
     );
